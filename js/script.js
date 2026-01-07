2278716a9bf2dd5722b31d484a97eecdfcefac4d
Amit Tiwari <amit067843@gmail.com>
2026-01-07 17:05:42 +0530

    fix the message section working on notice board and about us

--- js/script.js
+++ js/script.js
@@ -14,6 +14,34 @@ document.addEventListener("DOMContentLoaded", () => {
             clickable: true,
         },
     });
+
+
+    const progressCircle = document.querySelector(".autoplay-progress svg");
+    const progressContent = document.querySelector(".autoplay-progress span");
+
+    var swiper = new Swiper(".mySwiper2", {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 0,
+        navigation: {
+            nextEl: ".swiper-scnd-button-next",
+            prevEl: ".swiper-scnd-button-prev",
+        },
+        pagination: {
+            el: ".swiper-scnd-pagination",
+            clickable: true,
+        },
+        // autoplay: {
+        //     delay: 5000,
+        //     disableOnInteraction: false
+        // },
+        // on: {
+        //     autoplayTimeLeft(s, time, progress) {
+        //         progressCircle.style.setProperty("--progress", 1 - progress);
+        //         progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        //     }
+        // }
+    });
 });
 
 
